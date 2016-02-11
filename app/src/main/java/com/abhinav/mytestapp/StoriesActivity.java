34772b19e8db71242a290a478c6c1a6d672ed27e
abhinav272 <abhinav.ih@gmail.com>
2016-02-11 13:21:29 +0530

    Stories Activity added

--- app/src/main/java/com/abhinav/mytestapp/StoriesActivity.java
+++ app/src/main/java/com/abhinav/mytestapp/StoriesActivity.java
@@ -1,25 +1,16 @@
 package com.abhinav.mytestapp;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by abhinavsharma on 11-02-2016.
  */
-public class SplashScreen extends AppCompatActivity {
+public class StoriesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen_activity);
-
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-
-            }
-        },2000);
+        setContentView(R.layout.stories_activity);
     }
 }
