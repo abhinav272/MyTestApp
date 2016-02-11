f351130b01b41ce45e97a085dda9ec1be26dd599
abhinav272 <abhinav.ih@gmail.com>
2016-02-11 13:07:21 +0530

    Added splash screen

--- app/src/main/java/com/abhinav/mytestapp/SplashScreen.java
+++ app/src/main/java/com/abhinav/mytestapp/SplashScreen.java
@@ -1,7 +1,16 @@
 package com.abhinav.mytestapp;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by abhinavsharma on 11-02-2016.
  */
-public class SplashScreen {
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen_activity);
+    }
 }
